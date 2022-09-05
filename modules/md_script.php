c378c0b5835c9140858d977bf3529d73aee31bb6
vioris <30378435+vioris@users.noreply.github.com>
2022-09-05 22:14:06 +0300

    Add files via upload

---
+++ modules/md_script.php
@@ -0,0 +1,73 @@
+<script src="../js/breakpoints.js"></script>
+<script src="../js/jquery/jquery-1.11.1.min.js"></script>
+<!-- bootstrap -->
+<script src="../js/scrollspy.js"></script>
+<script src="../js/bootstrap-progressbar/bootstrap-progressbar.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<!-- end bootstrap -->
+<script src="../js/masonry.pkgd.min.js"></script>
+<script src='../js/imagesloaded.pkgd.min.js'></script>
+<!-- bxslider -->
+<script src="../js/bxslider/jquery.bxslider.min.js"></script>
+<!-- end bxslider -->
+<!-- flexslider -->
+<script src="../js/flexslider/jquery.flexslider.js"></script>
+<!-- end flexslider -->
+<!-- smooth-scroll -->
+<script src="../js/smooth-scroll/SmoothScroll.js"></script>
+<!-- end smooth-scroll -->
+<!-- carousel -->
+<script src="../js/jquery.carouFredSel-6.2.1-packed.js"></script>
+<!-- end carousel -->
+<script src="../js/rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+<script src="../js/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script src="../js/rs-plugin/videojs/video.js"></script>
+
+<!-- jquery ui -->
+<script src="../js/jqueryui/jquery-ui.js"></script>
+<!-- end jquery ui -->
+<script type="text/javascript" language="javascript"
+        src="http://maps.google.com/maps/api/js?sensor=false&key=AIzaSyCfVS1-Dv9bQNOIXsQhTSvj7jaDX7Oocvs"></script>
+<!-- Modules -->
+<script src="../js/modules/sliders.js"></script>
+<script src="../js/modules/ui.js"></script>
+<script src="../js/modules/retina.js"></script>
+<script src="../js/modules/animate-numbers.js"></script>
+<script src="../js/modules/parallax-effect.js"></script>
+<script src="../js/modules/settings.js"></script>
+<script src="../js/modules/maps-google.js"></script>
+<script src="../js/modules/color-themes.js"></script>
+<!-- End Modules -->
+
+<!-- Audio player -->
+<script type="text/javascript" src="../js/audioplayer/js/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="../js/audioplayer/js/jplayer.playlist.min.js"></script>
+<script src="../js/audioplayer.js"></script>
+<!-- end Audio player -->
+
+<!-- radial Progress -->
+<script src="../js/radial-progress/jquery.easing.1.3.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/d3/3.4.13/d3.min.js"></script>
+<script src="../js/radial-progress/radialProgress.js"></script>
+<script src="../js/progressbars.js"></script>
+<!-- end Progress -->
+
+<!-- Google services -->
+<script type="text/javascript" src="../https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['corechart']}]}"></script>
+<script src="../js/google-chart.js"></script>
+<!-- end Google services -->
+<script src="../js/j.placeholder.js"></script>
+
+<!-- Fancybox -->
+<script src="../js/fancybox/jquery.fancybox.pack.js"></script>
+<script src="../js/fancybox/jquery.mousewheel.pack.js"></script>
+<script src="../js/fancybox/jquery.fancybox.custom.js"></script>
+<!-- End Fancybox -->
+<script src="../js/user.js"></script>
+<script src="../js/timeline.js"></script>
+<script src="../js/fontawesome-markers.js"></script>
+<script src="../js/markerwithlabel.js"></script>
+<script src="/js/cookie.js"></script>
+<script src="../js/loader.js"></script>
+<script src="../js/scrollIt/scrollIt.min.js"></script>
+<script src="../js/modules/navigation-slide.js"></script>
