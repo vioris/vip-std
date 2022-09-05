e0551af4375b61c1b9f6179f289425f51324fed3
vioris <30378435+vioris@users.noreply.github.com>
2022-09-05 22:13:15 +0300

    Add files via upload

---
+++ pages/page_error_404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>404 page | Frexy Responsive HTML Template</title>
+   <?php include '../inc/head.php';?>
+</head>
+<body>
+<?php include'../inc/header.php';?>
+<div class="j-menu-container"></div>
+<div class="l-main-container">
+    <div class="b-page-error">
+        <div class="container">
+            <div class="b-title-error f-title-error">
+                <span class="f-primary-eb">404</span>
+                <strong class="f-primary-l">Page Not Found !</strong>
+            </div>
+            <div class="b-error-description f-error-description">
+                <strong class="f-primary">Sorry, this page does not exist</strong>
+                <span class="f-primary">The link you clicked might be corrupted, or the page may have been removed.</span>
+            </div>
+            <div class="b-error-search">
+                <div class="b-form-row b-input-search">
+                    <input class="form-control" type="text" placeholder="Enter your keyword" />
+                    <span class="b-btn b-btn-search f-btn-search fa fa-search"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include'../inc/footer.php';?>
+</body>
+</html>
---
+++ pages/progect.php
@@ -0,0 +1,22 @@
+<?php require '../config.php' ;?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Portfolio: Masonry | Frexy Responsive HTML Template</title>
+   <?php include '../modules/md_head.php';?>
+</head>
+<body>
+
+ <header>  
+        <?php include '../modules/md_header.php'; ?>
+       </header>
+<?php include '../modules/md_progect.php';?>
+
+<footer>
+  <?php include '../modules/md_footer.php';?>
+</footer>
+<?php include '../modules/md_script.php';?>
+
+</body>
+</html>
